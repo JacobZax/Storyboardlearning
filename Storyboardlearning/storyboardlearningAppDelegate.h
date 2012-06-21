845f77b5a23b2b27983656f5267441b4e3c0b76a
MacBook <MacBook@macbooks-macbook.local>
2012-06-21 14:11:41 -0600

    Initial Commit

---
+++ Storyboardlearning/storyboardlearningAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  storyboardlearningAppDelegate.h
+//  Storyboardlearning
+//
+//  Created by Aaron Holland-Plum on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface storyboardlearningAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Storyboardlearning/storyboardlearningViewController.h
@@ -0,0 +1,13 @@
+//
+//  storyboardlearningViewController.h
+//  Storyboardlearning
+//
+//  Created by Aaron Holland-Plum on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface storyboardlearningViewController : UIViewController
+
+@end
